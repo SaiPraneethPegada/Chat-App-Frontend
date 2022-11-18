ffe21094bf93f41785dbc07296d1860c9e473053
Pegada Sai Praneeth <saipraneethpegada@gmail.com>
2022-11-18 22:57:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/HomePage";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
+import "./App.css";
+// export const API_URL = "http://localhost:8000";
+export const API_URL = "https://chat-app-api-kaw0.onrender.com";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="*" exact element={<Login />} />
+    </Routes>
   );
 }
 
